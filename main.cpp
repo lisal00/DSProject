5a8001b67c5524836d651124bfb4aac57ad5aa1e
phucv2376 <phucv2376@gmail.com>
2023-11-28 21:21:07 -0600

    billy

--- main.cpp
+++ main.cpp
@@ -1,52 +1,83 @@
 #include <wx/wx.h>
+#include "infix.h"
+#include "binary.h"
+#include "calculator.h"
+#include "string"
+#include <iostream>
 
 class MyFrame : public wxFrame
 {
+private:
+	wxButton *calB;
+	wxButton *binB;
+	wxButton *postfixB;
+	wxStaticText *staticText;
+	wxTextCtrl *textBox;
+	calculator calcClass;
+	infix infix;
+
 public:
 	MyFrame(const wxString &title)
 		: wxFrame(NULL, wxID_ANY, title)
 	{
 
 		// Create a button
-		wxButton *calB = new wxButton(this, wxID_ANY, "Calculate", wxPoint(200, 50), wxSize(200, 60), 0);
-		wxButton *binB = new wxButton(this, wxID_ANY, "Binary Converter", wxPoint(450, 50), wxSize(200, 60), 0);
-		wxButton *postfixB = new wxButton(this, wxID_ANY, "Postfix Converter", wxPoint(700, 50), wxSize(200, 60), 0);
-		wxStaticText *staticText = new wxStaticText(this, wxID_ANY, "Enter an ex");
+		calB = new wxButton(this, wxID_ANY, "Calculate", wxPoint(200, 50), wxSize(200, 60), 0);
+		binB = new wxButton(this, wxID_ANY, "Binary Converter", wxPoint(450, 50), wxSize(200, 60), 0);
+		postfixB = new wxButton(this, wxID_ANY, "Postfix Converter", wxPoint(700, 50), wxSize(200, 60), 0);
 
-		// Bind a function to handle button click events
-		calB->Bind(wxEVT_BUTTON, &MyFrame::OnCalBClick, this);
-		// binB->Bind(wxEVT_BUTTON, &MyFrame::OnButtonClick, this);
-		this
+		// Bind event handlers to buttons
+		calB->Bind(wxEVT_BUTTON, &MyFrame::OnButton1Click, this);
+		binB->Bind(wxEVT_BUTTON, &MyFrame::OnButton2Click, this);
+		postfixB->Bind(wxEVT_BUTTON, &MyFrame::OnButton3Click, this);
+	}
 
-			// Event handler for button click
-			void
-			OnCalBClick(wxCommandEvent & event)
-		{
-
-			// Show an input box
-		}
-
-		// Declare the event table
-		wxDECLARE_EVENT_TABLE();
-	};
-
-	// Event table for MyFrame
-	wxBEGIN_EVENT_TABLE(MyFrame, wxFrame)
-		EVT_BUTTON(wxID_ANY, MyFrame::OnCalBClick)
-
-			wxEND_EVENT_TABLE()
-
-				class MyApp : public wxApp
+	// Event handlers for buttons
+	void OnButton1Click(wxCommandEvent &event)
 	{
-	public:
-		virtual bool OnInit()
-		{
-			MyFrame *frame = new MyFrame("Our very great project DU MA 他媽的");
-			frame->SetClientSize(1000, 500);
-			frame->Center();
-			frame->Show(true);
-			return true;
-		}
-	};
+		staticText = new wxStaticText(this, wxID_ANY, "Enter an expression: ", wxPoint(250, 250));
+		textBox = new wxTextCtrl(this, wxID_ANY, " ", wxPoint(375, 250), wxSize(200, 20));
+		wxStreamToTextRedirector redirect(textBox);
+		wxButton *calculateB = new wxButton(this, wxID_ANY, "=", wxPoint(575, 250), wxSize(100, 20), 0);
+		calculateB->Bind(wxEVT_BUTTON, &MyFrame::handleCalculate, this);
+	}
 
-	wxIMPLEMENT_APP(MyApp);
+	void OnButton2Click(wxCommandEvent &event)
+	{
+		staticText = new wxStaticText(this, wxID_ANY, "Enter an expression: ", wxPoint(150, 250));
+		wxTextCtrl *control = new wxTextCtrl(this, wxID_ANY, " ", wxPoint(300, 250));
+		wxStreamToTextRedirector redirect(control);
+	}
+
+	void OnButton3Click(wxCommandEvent &event)
+	{
+		staticText = new wxStaticText(this, wxID_ANY, "Enter an expression: ", wxPoint(150, 250));
+		wxTextCtrl *control = new wxTextCtrl(this, wxID_ANY, " ", wxPoint(300, 250));
+		wxStreamToTextRedirector redirect(control);
+	}
+
+	void handleCalculate(wxCommandEvent &event)
+	{
+		wxString expression = textBox->GetValue();
+		string expressionStr = string(expression.mb_str());
+		string postfix = infix.convert(expressionStr);
+		string res = calcClass.eval(postfix);
+		cout << res;
+	}
+};
+
+class MyApp : public wxApp
+{
+public:
+	virtual bool OnInit()
+	{
+		MyFrame *frame = new MyFrame("Our very great project DU MA 他媽的");
+		frame->SetClientSize(1000, 500);
+		frame->Center();
+		frame->Show(true);
+		frame->Show(true);
+		return true;
+	}
+};
+
+wxIMPLEMENT_APP(MyApp);
